ad9bb89575beebd2e81270c0f43223a5147463e6
Olle Hagner <olle.hagner@arbetsformedlingen.se>
2021-12-09 13:55:25 +0100

    Day 9 iterative basin search

--- src/day9/Day09.kt
+++ src/day9/Day09.kt
@@ -4,9 +4,14 @@ import common.Direction.*
 import common.Grid
 import common.Point
 import readInput
+import java.util.*
+import kotlin.collections.HashSet
 
+const val MAX_HEIGHT = 9
 
 fun main() {
+
+
 
     fun adjacentPoints(point: Point): List<Point> {
         return listOf(UP, DOWN, LEFT, RIGHT )
@@ -30,11 +35,21 @@ fun main() {
         println("Day 9 part 1. Sum of low points: $sumOfLowPoints")
     }
 
-    /*
-        0,0 ->
-     */
+    fun iterativeGetBasinPoint(grid: Grid<Int>, start: Point) : Set<Point> {
+        val basin = HashSet<Point>()
+        val pointsToCheck = LinkedList<Point>()
+        pointsToCheck.add(start)
+        do {
+            val current = pointsToCheck.pop()
+            if(grid.valueOrDefault(current, MAX_HEIGHT) < MAX_HEIGHT) {
+                basin.add(current)
+                pointsToCheck.addAll(adjacentPoints(current) - basin)
+            }
+        } while (pointsToCheck.isNotEmpty())
+        return basin
+    }
 
-    fun getBasinPoints(grid: Grid<Int>, pointsToCheck: Set<Point>) : Set<Point> {
+    fun recursiveGetBasinPoints(grid: Grid<Int>, pointsToCheck: Set<Point>) : Set<Point> {
         val adjacentBasinPoints = pointsToCheck
             .flatMap { adjacentPoints(it) }
             .filter { grid.hasValue(it) }
@@ -44,7 +59,7 @@ fun main() {
         if(adjacentBasinPoints.isEmpty()) {
             return pointsToCheck
         } else {
-            return getBasinPoints(grid, pointsToCheck + adjacentBasinPoints)
+            return recursiveGetBasinPoints(grid, pointsToCheck + adjacentBasinPoints)
         }
 
     }
@@ -52,7 +67,8 @@ fun main() {
     fun part2(input: List<List<Int>>) {
         val grid = Grid(input)
         val threeLargestBasinsProduct = getLowPoints(grid)
-            .map { lowpoint -> getBasinPoints(grid, setOf(lowpoint)) }
+            .map { lowpoint -> recursiveGetBasinPoints(grid, setOf(lowpoint)) }
+//            .map { lowpoint -> iterativeGetBasinPoint(grid, lowpoint) }
             .map { it.size }
             .sortedBy { it }
             .takeLast(3)
