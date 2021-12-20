a5469b121534342aaa87af816020b6e57dbe9a8c
Olle Hagner <olle.hagner@choice.se>
2021-12-20 14:09:57 +0100

    Day 17 continued

--- src/day17/Day17.kt
+++ src/day17/Day17.kt
@@ -1,8 +1,10 @@
 package day17
 
 import common.Point
+import readInput
+import java.lang.Math.abs
 
-typealias TargetArea=Pair<IntRange, IntRange>
+typealias TargetArea = Pair<IntRange, IntRange>
 
 fun main() {
 
@@ -12,7 +14,7 @@ fun main() {
 
     fun infinite(startValue: Int): Sequence<Int> = sequence {
         var value = startValue
-        while(true) {
+        while (true) {
             yield(value++)
         }
     }
@@ -55,29 +57,47 @@ fun main() {
             .toList()
     }
 
+    fun valuesInYSequence(range: IntRange): List<Int> {
+        return infinite(0)
+            .takeWhile { it < abs(range.first) }
+            .filter { ySequence(it).any { position -> position in range } }
+            .toList()
+    }
 
 
     fun part1(targetArea: TargetArea) {
-        val xValues = valuesInXSequence(targetArea.first)
-        val lastXValueInTargetArea = targetArea.first.last
-        xSequence(xValues.first(), lastXValueInTargetArea)
-            .zip(ySequence(3))
-            .map { (x, y) -> Point(x, y) }
-            .takeWhile { point -> point.x < targetArea.first.last && point.y > targetArea.second.last }
-            .forEach { println(it) }
+//        val xValues = valuesInXSequence(targetArea.first)
+//        val yValues = valuesInYSequence(targetArea.second)
+        val maxY = valuesInYSequence(targetArea.second).maxOf { it }
+        println("Day 17 part 1. Max height = ${gauss(maxY)}")
+//        val lastXValueInTargetArea = targetArea.first.last
+//        xSequence(xValues.first(), lastXValueInTargetArea)
+//            .zip(ySequence(3))
+//            .map { (x, y) -> Point(x, y) }
+//            .takeWhile { point -> point.x < targetArea.first.last && point.y > targetArea.second.last }
+//            .forEach { println(it) }
 
     }
 
+    fun part2(targetArea: TargetArea) {
+        val xValues = valuesInXSequence(targetArea.first)
+        val yValues = valuesInYSequence(targetArea.second)
+        println(xValues)
+        println(yValues)
+        println("Size = ${xValues.size * yValues.size}")
+    }
+
     val testInput = "target area: x=20..30, y=-10..-5"
+    val input = readInput("day17/day17").first()
 
     val ranges = ".*x=(-?\\d+)\\.\\.(-?\\d+).*y=(-?\\d+)\\.\\.(-?\\d+)".toRegex()
     val groups = ranges.matchEntire(testInput)!!.groupValues
-
-
     val xRange = IntRange(groups[1].toInt(), groups[2].toInt())
     val yRange = IntRange(groups[3].toInt(), groups[4].toInt())
     val targetArea = TargetArea(xRange, yRange)
-    part1(targetArea)
+
+//    part1(targetArea)
+    part2(targetArea)
 
 
 }
