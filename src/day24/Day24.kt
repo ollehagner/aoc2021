e7f0c5b77e477952d615aba622db582e43732494
Olle Hagner <ohagner@Kuma.local>
2022-01-17 09:00:49 +0100

    WIP day 24

--- src/day24/Day24.kt
+++ src/day24/Day24.kt
@@ -1,16 +1,46 @@
 package day24
 
+import java.lang.IllegalArgumentException
+
 fun main() {
 
 }
 
-class ALU(val instructions: List<String>) {
+class ALU(val instructions: List<String>, val inputValues: Iterator<Long>) {
 
-    val data = mapOf("w" to 0L, "x" to 0L, "y" to 0L, "z" to 0L)
+    val data = mutableMapOf("w" to 0L, "x" to 0L, "y" to 0L, "z" to 0L)
+
+    fun execute(command: String) {
+        val commandParts = command.split(" ")
+        val commandName = commandParts[0]
+        when(commandName) {
+            "inp" -> write(command.substringAfter(" "), inputValues.next())
+            "add" -> {
+                val a = commandParts[1]
+                val b = commandParts[2]
+                write(a, read(a) + read(b))
+            }
+            "mul" -> {
+                val a = commandParts[1]
+                val b = commandParts[2]
+                write(a, read(a) * read(b))
+            }
+            "div" -> {
+                val a = commandParts[1]
+                val b = commandParts[2]
+                write(a, read(a) / read(b))
+            }
+        }
+    }
 
     fun read(variableOrValue: String): Long {
         return data.getOrDefault(variableOrValue, variableOrValue.toLong())
     }
 
-    fun write()
+    private fun write(variable: String, value: Long) {
+        if(!data.containsKey(variable)) {
+            throw IllegalArgumentException("Key $variable doesn't exist")
+        }
+        data[variable] = value
+    }
 }
